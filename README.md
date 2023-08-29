1b1d8aa42254f4cba724debee20085b3b146b20b
MarcosAurelio <aurelioleonel@gmail.com>
2023-08-29 12:24:58 -0300

    Explorando IA Generativa em um Pipeline de ETL com Python

---
+++ README.md
@@ -0,0 +1,11 @@
+Este projeto exemplifica um processo de extração, transformação e carregamento (ETL) de dados utilizando Python. O objetivo é realizar a leitura de um arquivo CSV contendo informações de clientes, aplicar transformações nestes dados e convertê-los para o formato JSON.
+
+Inicialmente o arquivo CSV é lido utilizando a biblioteca Pandas, armazenando os dados em um DataFrame. Esta etapa representa a extração dos dados da fonte CSV.
+
+Em seguida, algumas transformações são aplicadas nos dados como conversão do tipo de dados de algumas colunas e renomeação de cabeçalhos. Estas transformações limpam e preparam os dados para analise.
+
+Os dados do DataFrame são então convertidos para o formato JSON, primeiro criando um dicionário Python a partir dos dados, para então serializar este dicionário em uma string JSON formatada e indentada. Esta etapa de conversão para JSON representa o carregamento dos dados estruturados em um novo formato.
+
+Foram utilizadas as bibliotecas Pandas para extração e transformação dos dados tabulares, e o módulo nativo JSON da linguagem Python para serialização dos dicionários em JSON.
+
+O processo realizado demonstra as principais etapas de um pipeline ETL utilizando Python, desde a extração dos dados brutos até o carregamento dos dados tratados em um formato desejado. As técnicas apresentadas podem ser expandidas para casos mais complexos de ETL.
---
+++ main.py
@@ -0,0 +1,80 @@
+# Projeto ETL
+# Santader Dev Week 2023
+# Marcos Aurélio Leonel
+
+# Importando as bibliotecas nescessarias
+import pandas as pd
+import json
+
+import csv
+
+# Carregando os dados do arquivo
+df = pd.read_csv('dados/clientes.csv')
+print(df.to_string()+"\n")  # exibindo em formato de tabela
+
+
+# Localizando o registro pelo 'id'
+def get_user(user_id):
+    usuario = df.loc[df['User_ID'] == user_id]
+
+    # Converte o DataFrame para dicionário
+    dict_usuario = usuario.to_dict('records')
+
+    # Converte o dicionário para JSON
+    json_usuario = json.dumps(dict_usuario[0], indent=2)
+
+    return json_usuario
+
+
+print(get_user(10)+"\n")
+
+
+# Atualizando o registro pelo 'id'
+def put_user(user_id, novo_nome, novo_email):
+    # Encontra o índice da linha correspondente ao usuário com o User_ID fornecido
+    indice = df[df['User_ID'] == user_id].index[0]
+
+    # Atualiza o nome e o e-mail do usuário no DataFrame
+    df.loc[indice, 'Nome'] = novo_nome
+    df.loc[indice, 'Email'] = novo_email
+
+    # Salva as alterações no arquivo CSV
+    df.to_csv("dados/clientes.csv", encoding='utf-8-sig', index=False)
+
+    return "Usuário atualizado com sucesso."
+
+
+# Atualiza o nome e o e-mail do usuário com User_ID = 10
+user_id = 10
+novo_nome = "Marcos Aurelio Leonel"
+novo_email = "etlcompython@python.com"
+resultado = put_user(user_id, novo_nome, novo_email)
+
+print(resultado+"\n")
+
+
+# listando todos os registro em formato json
+def get_all_user_json():
+    # df = pd.read_csv('clientes.csv')
+
+    # Converte o dataframe inteiro para dicionário
+    data = df.to_dict('records')
+
+    # Converte o dicionário para JSON
+    json_data = json.dumps(data, indent=2)
+
+    print(json_data)
+
+
+get_all_user_json()
+
+
+# Pode ser usado com for
+
+# for index, row in df.iterrows():
+#     # Converte cada linha para dicionário
+#     linha = row.to_dict()
+#
+#     # Converte para JSON e imprime
+#     json_row = json.dumps(linha, indent=2)
+#     print(json_row)
